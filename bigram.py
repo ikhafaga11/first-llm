d710a6d74b1e654924f0563a743618a551d2daa3
Issa Khafaga <ikhafaga@live.com>
2026-03-17 16:59:52 +0000

    feat: increase optimisation through change in hyper params

--- bigram.py
+++ bigram.py
@@ -91,8 +91,9 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B,T) array of indices in the current context
         for _ in range(max_new_tokens):
+            idx_cond = idx[:, -block_size:]
             # get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
             # focus only on the last time step
             logits = logits[:, -1, :] # becomes (B,C)
             #apply softmax to get probabilities
