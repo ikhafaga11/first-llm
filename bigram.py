027dfa659f6fb06b4af8ee5a02745e40ddc36003
Issa Khafaga <ikhafaga@live.com>
2026-03-17 15:10:02 +0000

    feat: add self attention layer

--- bigram.py
+++ bigram.py
@@ -74,7 +74,7 @@ class BigramLanguageModel(nn.Module):
  
         #idx and targets are bot (B,T) tensor of integers
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
-        pos_emb = self.position_embedding_table(torch.arrange(T, device = device)) # (T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device = device)) # (T,C)
         x = tok_emb + pos_emb
         logits = self.lm_head(x) # (B,T,vocab_size)
 
